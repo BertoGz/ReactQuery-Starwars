78c366dbb139d382960dc40f97c65e5747cef86d
bertogz <bertogzlx@gmail.com>
2022-02-27 12:51:28 -0800

    correct import, clean up

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,8 +1,9 @@
 import { useQuery } from "react-query";
 import { GET_PEOPLE_ENDPOINT } from "./Constants";
-import Person  from "./Planet";
+import Person from "./Person";
 const fetchPeople = async () => {
   const res = await fetch(GET_PEOPLE_ENDPOINT); // fetch data from server
+
   return res.json(); // return the json data
 };
 const People = () => {
@@ -13,9 +14,13 @@ const People = () => {
         <h2>People</h2>
         {status === "loading" && <div>Loading...</div>}
         {status === "error" && <div>Error</div>}
-        {status === "success"  && <div>{data?.results?.map((p)=>{
-            return <Person {...{key:p?.name,person:p}}/>
-        })}</div>} 
+        {status === "success" && (
+          <div>
+            {data?.results?.map((person) => {
+              return <Person {...{ person }} />;
+            })}
+          </div>
+        )}
       </div>
     </>
   );
