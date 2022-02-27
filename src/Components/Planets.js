469619b98859e3e815a696d982e93b51681e8a39
bertogz <bertogzlx@gmail.com>
2022-02-27 12:09:15 -0800

    planets pulls planet data

--- src/Components/Planets.js
+++ src/Components/Planets.js
@@ -1,15 +1,21 @@
 import { useQuery } from "react-query";
 import { GET_PLANETS_ENDPOINT } from "./Constants";
-const fetchPlanets = async()=>{
-    const res = await fetch(GET_PLANETS_ENDPOINT)  // fetch data from server
-    return res.json() // return the json data
-}
+import Planet from "./Planet";
+const fetchPlanets = async () => {
+  const res = await fetch(GET_PLANETS_ENDPOINT); // fetch data from server
+  return res.json(); // return the json data
+};
 const Planets = () => {
-    const {} = useQuery('planets',fetchPlanets)
+  const { data, status } = useQuery("planets", fetchPlanets);
   return (
     <>
       <div>
         <h2>Planets</h2>
+        {status === "loading" && <div>Loading...</div>}
+        {status === "error" && <div>Error</div>}
+        {status === "success"  && <div>{data?.results?.map((planet)=>{
+            return <Planet {...{planet}}/>
+        })}</div>}
       </div>
     </>
   );
