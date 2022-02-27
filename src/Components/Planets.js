24b7639bd45333823fbea363dd40cbe7fc6f71f6
bertogz <bertogzlx@gmail.com>
2022-02-27 12:07:58 -0800

    add fetching of planets

--- src/Components/Planets.js
+++ src/Components/Planets.js
@@ -1,4 +1,11 @@
+import { useQuery } from "react-query";
+import { GET_PLANETS_ENDPOINT } from "./Constants";
+const fetchPlanets = async()=>{
+    const res = await fetch(GET_PLANETS_ENDPOINT)  // fetch data from server
+    return res.json() // return the json data
+}
 const Planets = () => {
+    const {} = useQuery('planets',fetchPlanets)
   return (
     <>
       <div>
