2348fcfc568d42ae7d24b755fdaca822c5903a9f
bertogz <bertogzlx@gmail.com>
2022-02-27 12:08:20 -0800

    add baseUrl

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http swapi.dev/api'
+const BASE_URL = 'http://swapi.dev/api'
 export const GET_PLANETS_ENDPOINT = `${BASE_URL}/planets`
 export const GET_PEOPLE_ENDPOINT = `${BASE_URL}/people`
