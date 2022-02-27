5c3f11f00808e50c637925bbfc1a6f465556a4cc
bertogz <bertogzlx@gmail.com>
2022-02-27 12:08:47 -0800

    continue initial Jsx

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,8 +1,21 @@
+import { useQuery } from "react-query";
+import { GET_PEOPLE_ENDPOINT } from "./Constants";
+import Person  from "./Planet";
+const fetchPeople = async () => {
+  const res = await fetch(GET_PEOPLE_ENDPOINT); // fetch data from server
+  return res.json(); // return the json data
+};
 const People = () => {
+  const { data, status } = useQuery("people", fetchPeople);
   return (
     <>
       <div>
         <h2>People</h2>
+        {status === "loading" && <div>Loading...</div>}
+        {status === "error" && <div>Error</div>}
+        {status === "success"  && <div>{data?.results?.map((p)=>{
+            return <Person {...{key:p?.name,person:p}}/>
+        })}</div>} 
       </div>
     </>
   );
